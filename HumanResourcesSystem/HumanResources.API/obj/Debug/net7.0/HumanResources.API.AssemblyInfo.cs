5401993112f406778c3451589270b1d481542b5a
Jakub Marczak <jj.marczak98@gmail.com>
2024-05-19 22:09:59 +0200

    Integration test

--- HumanResourcesSystem/HumanResources.API/obj/Debug/net7.0/HumanResources.API.AssemblyInfo.cs
+++ HumanResourcesSystem/HumanResources.API/obj/Debug/net7.0/HumanResources.API.AssemblyInfo.cs
@@ -18,6 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyProductAttribute("HumanResources.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HumanResources.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("HumarnResource.IntegrationTest")]
 
 // Wygenerowane przez klasę WriteCodeFragment programu MSBuild.
 
