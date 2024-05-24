c2e8bd1251a6848a86c429330ad0df1156a43b2c
Jakub Marczak <jj.marczak98@gmail.com>
2024-05-24 11:01:34 +0200

    Test

--- HumanResourcesSystem/HumarnResource.IntegrationTest/FakeDatabaseSeeder/FakeDatabaseSeeder.cs
+++ HumanResourcesSystem/HumarnResource.IntegrationTest/FakeDatabaseSeeder/FakeDatabaseSeeder.cs
@@ -5,48 +5,40 @@ namespace HumarnResource.IntegrationTest.FakeDatabaseSeeder
 {
     public class FakeDatabaseSeeder
     {
-        private readonly HumanResourcesDatabase _database;
-        public FakeDatabaseSeeder(HumanResourcesDatabase database) 
-        {
-            _database = database;
-        }
 
-        public async Task Seed()
-        {
-            if(await  _database.Database.CanConnectAsync()) 
+        public static User GetUser() =>
+            new()
             {
-                if(!_database.Users.Any()) 
+                Id = "100",
+                Email = "test@example.com",
+                UserCode = "0000000000000",
+                UserName = "test@example.com",
+                EmailConfirmed = false,
+                PhoneNumberConfirmed = false,
+                TwoFactorEnabled = false,
+                LockoutEnabled = false,
+                AccessFailedCount = 0,
+                RefreshToken = "0000",
+                UserInfo = new UserInfo()
                 {
-                    await _database.Users.AddAsync(new User()
-                    {
-                        Id="100",
-                        Email = "test@example.com",
-                        UserCode="0000000000000",
-                        UserName = "test@example.com",
-                        EmailConfirmed = false,
-                        PhoneNumberConfirmed = false,
-                        TwoFactorEnabled = false,
-                        LockoutEnabled = false,
-                        AccessFailedCount = 0,
-                        RefreshToken = "0000",
-                        UserInfo = new UserInfo()
-                        {
-                            UserId = "100",
-                            Name = "Test",
-                            LastName = "Test",
-                            UserCode = "0000000000000",
-                            DaysOfAbsencesToUse = 28,
-                            DaysOfAbsencesCurrentYear = 0,
-                            YearsOfExperiences = 2,
-                            EducationTitle = HumanResources.Domain.Enums.EducationLevel.Higher,
-                            DepartmentID = 9
+                    UserId = "100",
+                    Name = "Test",
+                    LastName = "Test",
+                    UserCode = "0000000000000",
+                    DaysOfAbsencesToUse = 28,
+                    DaysOfAbsencesCurrentYear = 0,
+                    YearsOfExperiences = 2,
+                    EducationTitle = HumanResources.Domain.Enums.EducationLevel.Higher,
+                    DepartmentID = 9
 
-                        }
-
-                    });
                 }
-            }
-            await _database.SaveChangesAsync();
-        }
+            };
+
+        public static Roles GetRoles() =>
+            new()
+            {
+                Name = "User",
+                NormalizedName = "USER",
+            };
     }
 }
