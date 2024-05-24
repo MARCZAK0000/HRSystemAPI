3cef70f0ec593459c5839d30e024379db5d227c4
Jakub Marczak <jj.marczak98@gmail.com>
2024-05-24 11:10:00 +0200

    Update

---
+++ HumanResourcesSystem/HumarnResource.IntegrationTest/TestFactory.cs
@@ -0,0 +1,48 @@
+﻿using HumanResources.Infrastructure.Database;
+using HumarnResource.IntegrationTest.Authentication;
+using HumarnResource.IntegrationTest.FakeDatabaseSeeder;
+using Microsoft.AspNetCore.Authorization.Policy;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace HumarnResource.IntegrationTest
+{
+    internal class TestFactory : WebApplicationFactory<Program>
+    {
+        protected override async void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureTestServices(async services =>
+            {
+                //Remove current database
+                services.RemoveAll(typeof(DbContextOptions<HumanResourcesDatabase>));
+
+                //Connect to database
+                //services.AddSqlServer<HumanResourcesDatabase>("server=LAPTOP-AGSJTTQJ; database = TestHumanResourcesDb;trusted_connection=true;encrypt=false;");
+
+                services.AddSingleton<IPolicyEvaluator, FakePolicyEvaluator>();
+
+                services.AddMvc(opt => opt.Filters.Add(new FakeUserFilter()));
+
+                services.AddDbContext<HumanResourcesDatabase>(options =>
+                {
+                    options.UseInMemoryDatabase("HumanResourcesDatabase");
+
+                });
+                services.AddScoped<FakeDatabaseSeeder.FakeDatabaseSeeder>();
+
+                var serviceProvider = services.BuildServiceProvider();
+                var scope = serviceProvider.CreateScope();
+                var dbContext = scope.ServiceProvider.GetService<FakeDatabaseSeeder.FakeDatabaseSeeder>();
+                await dbContext.Seed();
+
+            });
+
+        }
+
+
+        
+
+    }
+}
---
+++ HumanResourcesSystem/HumarnResource.IntegrationTest/WebApplicationFactoryBuilder.cs
@@ -0,0 +1,64 @@
+﻿using HumanResources.Infrastructure.Database;
+using HumarnResource.IntegrationTest.Authentication;
+using HumarnResource.IntegrationTest.FakeDatabaseSeeder;
+using Microsoft.AspNetCore.Authorization.Policy;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace HumarnResource.IntegrationTest
+{
+    public class WebApplicationFactoryBuilder:IAsyncLifetime
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+        public HttpClient _client;
+        public WebApplicationFactoryBuilder()
+        {
+            _factory = new WebApplicationFactory<Program>().
+                WithWebHostBuilder(builer => 
+                    builer.ConfigureServices(services =>
+                {
+                    services.RemoveAll(typeof(DbContextOptions<HumanResourcesDatabase>));
+
+
+                    services.AddSingleton<IPolicyEvaluator, FakePolicyEvaluator>();
+
+                    services.AddMvc(opt => opt.Filters.Add(new FakeUserFilter()));
+
+
+                    services.AddDbContext<HumanResourcesDatabase>(options =>
+                    {
+                        options.UseInMemoryDatabase("HumanResourcesDatabase");
+
+                    });
+                    services.AddScoped<FakeDatabaseSeeder.FakeDatabaseSeeder>();
+                }));
+
+
+            _client=_factory.CreateClient();
+        }
+
+
+
+        public async Task DisposeAsync()
+        {
+            using var scope = _factory.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<HumanResourcesDatabase>();
+            
+            await context.Database.EnsureDeletedAsync();
+            
+        }
+
+        public async Task InitializeAsync()
+        {
+            using var scope = _factory.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<HumanResourcesDatabase>();
+            await context.Database.EnsureCreatedAsync();
+            //await context.Roles.AddAsync(FakeDatabaseSeeder.FakeDatabaseSeeder.GetRoles());
+            await context.Users.AddAsync(FakeDatabaseSeeder.FakeDatabaseSeeder.GetUser());
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
